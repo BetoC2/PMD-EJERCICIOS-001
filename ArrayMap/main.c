1e76c4c89de22b9c7d1f54751c97e27e6a218911
BetoC2 <renteria.alberto.c@gmail.com>
2022-09-12 20:12:17 -0500

    ArrayMap listo

--- ArrayMap/main.c
+++ ArrayMap/main.c
@@ -6,21 +6,26 @@
 //TEMPLATE BEGIN
 void almostMap(int *domain, int count)
 {
+    for (int i = 0; i < count; i++)
+    {
+        int num = *(domain + i);
+        *(domain + i) = num * num;
+    }
 }
 //TEMPLATE END
 
 //APPEND BEGIN
 int main() {
-  int *target = malloc(sizeof(int) * 1000000);
-  int count = 0;
-  fscanf(stdin, "%d\n", &count);
-  for(int i=0; i < count; i++)
-    fscanf(stdin, "%d,", target+i);
+    int *target = malloc(sizeof(int) * 1000000);
+    int count = 0;
+    fscanf(stdin, "%d\n", &count);
+    for(int i = 0; i < count; i++)
+        fscanf(stdin, "%d,", target+i);
   
-  almostMap(target, count);
-  for(int i=0; i < count; i++)
-    printf("%d,",target[i]);
-  free(target);
-  return 0;
+    almostMap(target, count);
+    for(int i=0; i < count; i++)
+        printf("%d,",target[i]);
+    free(target);
+    return 0;
 }
 //APPEND END
